ac90d60606a673ae756f1604c4ae61f5e25755e9
karthik309k <137162363+karthik309k@users.noreply.github.com>
2025-07-26 13:03:56 +0530

    Update and rename Fontend files to Header.jsx
    Frontend files

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className='bg-blue-600 text-white p-4 text-center text-xl font-bold'>
+      Bank Lending System
+    </header>
+  )
+}
+
+export default Header
